6a99f52e5bf5f07ba6038ef96efe067fb0f10137
e <e@ecorp.com>
2018-04-23 22:10:20 -0500

    add specs to post route

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,2 +1,3 @@
 class Make < ApplicationRecord
+  validates :name, presence: true
 end
--- spec/requests/makes_spec.rb
+++ spec/requests/makes_spec.rb
@@ -5,8 +5,28 @@ RSpec.describe "Makes", type: :request do
     it "works! (now write some real specs)" do
       get makes_path
       json = JSON.parse(response.body)
-      expect(json[0]).to include({"name" => "Ford"})
       expect(response).to have_http_status(200)
+      expect(json[0]).to include({"name" => "Ford"})
+    end
+  end
+  describe "GET /makes/:id" do
+    it "should the make at :id" do
+      get make_path(1)
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(json).to include({"name" => "Ford"})
+    end
+  end
+  describe "POST /makes" do
+    it "should create a new make" do
+      post makes_path, params: {make: {name: "Nissan"}}
+      expect(response).to have_http_status(201) 
+      
+      expect(Make.where({name: "Nissan"}).first).to be_truthy 
+    end
+    it "should be bad if not name provided" do
+      post makes_path, params: {make: { stuff: "things"}}
+      expect(response).to have_http_status(422)
     end
   end
 end
