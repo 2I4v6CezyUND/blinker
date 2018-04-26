513db5a5a02e704b3c3afa538927c520525157c3
e <e@ecorp.com>
2018-04-25 20:14:51 -0500

    complete makes spec

--- spec/requests/makes_spec.rb
+++ spec/requests/makes_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Makes", type: :request do
+  let!(:ford) { FactoryBot.create(:make, name: "Ford")}
+  let(:ford_json){ JSON.parse(ford.to_json) }
+
   describe "GET /makes" do
-    it "works! (now write some real specs)" do
+    it "should return a list of makes" do
       get makes_path
       json = JSON.parse(response.body)
       expect(response).to have_http_status(200)
-      expect(json[0]).to include({"name" => "Ford"})
+      expect(json).to include(ford_json)
     end
   end
+
   describe "GET /makes/:id" do
     it "should the make at :id" do
       get make_path(1)
@@ -17,6 +21,7 @@ RSpec.describe "Makes", type: :request do
       expect(json).to include({"name" => "Ford"})
     end
   end
+
   describe "POST /makes" do
     it "should create a new make" do
       post makes_path, params: {make: {name: "Nissan"}}
@@ -24,9 +29,26 @@ RSpec.describe "Makes", type: :request do
       
       expect(Make.where({name: "Nissan"}).first).to be_truthy 
     end
-    it "should be bad if not name provided" do
+    it "should validate  name" do
       post makes_path, params: {make: { stuff: "things"}}
       expect(response).to have_http_status(422)
+    end
+  end
+
+  describe "PUT /makes" do
+    it "should update the name" do
+      supermake = FactoryBot.create(:make, name: "Supermake")
+      put make_path(supermake.id), params: {make: {name: "NotSuperMake"}}
+      expect(response).to have_http_status(200)
+      expect(Make.where({name: "NotSuperMake"}).first).to be_truthy
+    end
+  end
+
+  describe "DELETE /makes" do
+    it "should delete the make" do
+      delete make_path(1)
+      expect(response).to have_http_status(204)
+      expect(Make.where({name: "Ford"}).first).to_not be_truthy
     end
   end
 end
